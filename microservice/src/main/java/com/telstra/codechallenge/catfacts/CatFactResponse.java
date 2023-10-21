005fa34344b38e26c40e3e3a3003bf4344b91124
Padmanaban A <PA20303803@wipro.com>
2023-10-21 17:39:13 +0530

    updated git endpoint and response

--- microservice/src/main/java/com/telstra/codechallenge/catfacts/CatFactResponse.java
+++ microservice/src/main/java/com/telstra/codechallenge/catfacts/CatFactResponse.java
@@ -3,7 +3,6 @@ package com.telstra.codechallenge.catfacts;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import lombok.Data;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class CatFactResponse {
